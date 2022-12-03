c08781b5604dbea76bdf8a26d08f7f3289865068
iljic <iljic@kth.se>
2022-12-03 20:19:41 +0100

    added genre search

--- src/Components/SideBar/genrePresenter.js
+++ src/Components/SideBar/genrePresenter.js
@@ -1,3 +1,28 @@
-function Genre(){
+import React from "react";
+import resolvePromise from "../../Utilities/resolvePromise";
+import promiseNoData from "../../Utilities/promiseNoData";
+import {getMovieByGenre} from "../../Utilities/dataSource";
+import GenreView from "./genreView"
+
+export default
+function Genre(props){
+    console.log(props);
+    const [promiseState]=  React.useState(function initialSearchACB(){return getMovieByGenre(props.model.currentGenre)})
+    const [, reRender]= React.useState(); 
+    React.useEffect( componentWasCreatedACB, [] ); 
     
+    function componentWasCreatedACB(){  
+        resolvePromise(getMovieByGenre(props.model.currentGenre), promiseState, notifyACB)
+        function isTakenDownACB(){           
+            
+        }
+       return isTakenDownACB;    
+    }
+    function notifyACB(){ reRender(new Object()); }
+
+    if(!promiseState.promise){
+        (resolvePromise(getMovieByGenre(props.model.currentGenre), promiseState, notifyACB))
+    }
+    return (<div>{promiseNoData(promiseState) || <GenreView genreResults={promiseState.data}/>}
+    </div>);
 }
--- src/Components/SideBar/genreView.js
+++ src/Components/SideBar/genreView.js
@@ -1,3 +1,16 @@
 function GenreView(props){
-
+    function searchResultCB(item){
+        function searchResultsClickACB(){
+          console.log("image clicked");
+        }
+        return <span key={item.id} class= "searchResult" onClick={searchResultsClickACB}>
+        <img src={"https://image.tmdb.org/t/p/w500/"+item.poster_path} height="150"/><div >{item.title}</div></span>
+    }
+    return (
+        <div>
+            {props.genreResults.map(searchResultCB)}
+        </div>
+    )
 }
+
+export default GenreView;
