3ba9020cf82a56607947f887914b4d0aab151f1f
iljic <iljic@kth.se>
2022-12-03 21:11:00 +0100

    hash changes

--- src/Components/App/app.js
+++ src/Components/App/app.js
@@ -4,14 +4,18 @@ import MainPagePresenter from '../MainPage/mainPagePresenter'
 import SearchbarPresenter from "../SearchBar/searchbarPresenter";
 import Sidebar from "../SideBar/sidebarPresenter";
 import TopMovies from '../SideBar/topp100MoviesPresenter';
+import Genre from '../SideBar/genrePresenter';
 function App(props) {
+  console.log(props.model.currentGenre);
   return (
     <div className="App">
       <div><Sidebar model = {props.model}/></div>
-      <Show hash="#Top100"><TopMovies/></Show>
+      <Show hash="#top100"><TopMovies/></Show>
       <div className="searchResult">
-      <div><SearchbarPresenter model = {props.model}></SearchbarPresenter></div>
-      <Show hash="#mainContent"><MainPagePresenter model = {props.model}/></Show>
+      <div>
+        <SearchbarPresenter model = {props.model}></SearchbarPresenter></div>
+        <Show hash="#mainContent"><MainPagePresenter model = {props.model}/></Show>
+        <Show hash={"#genre"}><Genre model = {props.model}/></Show>
       </div>
     </div>
   );
