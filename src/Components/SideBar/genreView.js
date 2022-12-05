a5c574e1cd27d08d9c816fb4130f0d2d8ce738ac
iljic <iljic@kth.se>
2022-12-05 09:51:49 +0100

    fixed missing titles from tv shows

--- src/Components/SideBar/genreView.js
+++ src/Components/SideBar/genreView.js
@@ -1,10 +1,11 @@
 function GenreView(props){
+    console.log(props);
     function searchResultCB(item){
         function searchResultsClickACB(){
           console.log("image clicked");
         }
         return <span key={item.id} class= "searchResult" onClick={searchResultsClickACB}>
-        <img src={"https://image.tmdb.org/t/p/w500/"+item.poster_path} height="150"/><div >{item.title}</div></span>
+        <img src={"https://image.tmdb.org/t/p/w500/"+item.poster_path} height="150"/><div >{item.title === undefined ? item.name : item.title}</div></span>
     }
     return (
         <div>
