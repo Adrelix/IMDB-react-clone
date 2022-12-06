cbeaacfac6c6a0b5a254ded843efd3c508544d30
iljic <iljic@kth.se>
2022-12-06 16:06:29 +0100

    some improvements to the API-calls

--- src/Utilities/dataSource.js
+++ src/Utilities/dataSource.js
@@ -18,7 +18,7 @@ async function getMovieDetails(movieID){
 }
 async function searchMovies(paramsObj){
     const searchParams = new URLSearchParams(paramsObj);
-    const endpoint = "search/movie?api_key=" + API_KEY + "&" + searchParams;
+    const endpoint = "search/movie?api_key=" + API_KEY + "&language=en-US&" + searchParams + "&page=1&include_adult=false";
     console.log(BASE_URL+endpoint);
     return fetch(BASE_URL+endpoint, options).then(treatHTTPResponseACB).then(transformResultCB);
 }
@@ -31,12 +31,12 @@ async function searchPerson(paramsObj){
 }
 
 async function getTopRated(){
-    const endpoint = "movie/top_rated?api_key=" + API_KEY;
+    const endpoint = "movie/top_rated?api_key=" + API_KEY + "&language=en-US&page=1&region=SE";
     return fetch(BASE_URL+endpoint, options).then(treatHTTPResponseACB).then(transformResultCB);
 }
 
 async function getPopular(){
-    const endpoint = "movie/popular?api_key=" + API_KEY;
+    const endpoint = "movie/popular?api_key=" + API_KEY + "&language=en-US&page=1&region=GB";
     return fetch(BASE_URL+endpoint, options).then(treatHTTPResponseACB).then(transformResultCB);
 }
 
@@ -53,7 +53,7 @@ async function getMovieByGenre(genreID, mediaType, page){
         mediaType +
         "?api_key=" +
         API_KEY +
-        "&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=" +
+        "&language=en-US&sort_by=vote_average.desc&certification_country=US&certification.lte=R&include_adult=false&include_video=false&page=" +
         page +
         "&vote_count.gte=200&with_genres=" +
         genreID;
